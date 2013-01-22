2aeef78804042a0a487be634dd8ca5b8ee560395
Keith Burdis <keith@burdis.org>
2013-01-22 18:36:38 +0000

    Use augeas to change server entries leaving the rest of the config file unchanged.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,16 +2,9 @@ class ntp::config (
   $servers, # array
   $iburst,  # bool
 ) {
-  # defaults
-  File {
-    owner => 'root',
-    group => 'root',
-  }
-  file { '/etc/ntp.conf':
-    ensure  => present,
-    mode    => '0444',
-    content => template('ntp/ntp.conf.erb'),
-    require => Class['ntp::install'],
-    notify  => Class['ntp::service'],
+  augeas { 'ntp-servers':
+    changes => template('ntp/changes.erb'),
+    incl    => '/etc/ntp.conf',
+    lens    => 'Ntp.lns',
   }
 }
